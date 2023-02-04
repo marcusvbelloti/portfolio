0d3928b331c28b923fa8ff56d27d32b0be90b098
marcus.batista <marcusvbelloti@outlook.com>
2023-02-04 19:11:06 -0300

    organizando repo

--- front/index.js
+++ front/index.js
@@ -4,6 +4,7 @@
 
 let countEl = document.getElementById("count-el")
 let count = 0 //inicializa a variável
+let saveEl = document.getElementById("save-el")
 
 function increment() {
     count = count + 1
@@ -11,8 +12,15 @@ function increment() {
     console.log(count)
 }
 
-function subtraction(){
+function delNumber(){
     count = count - 1
     countEl.innerText = count
     console.log(count)
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent = countStr
+    countEl.textContent = 0
+    count = 0
 }
